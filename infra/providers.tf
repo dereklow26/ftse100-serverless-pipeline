51a801461ad929815e8b69f16aa501513e3ac5d5
dereklow26 <derek.low26@gmail.com>
2025-09-02 16:06:09 +0800

    Create providers.tf

---
+++ infra/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"           # pick your region
+  # If you use named profile: export AWS_PROFILE=yourprofile
+}
